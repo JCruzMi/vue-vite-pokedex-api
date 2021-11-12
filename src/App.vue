b64f4cc80ddf338c0f1f8ed1e91909d3f1bf978e
JCruzMi <47216265+JCruzMi@users.noreply.github.com>
2021-11-12 17:40:29 -0500

    pokeapi implementation

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,40 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1>Pokedex</h1>
+  <input type="text" v-model="filterText">
+  <ul>
+    <li v-for="(pokemon, index) in pokeStore.filteredList" 
+      :key="`poke-${index}`"
+    >
+      #{{ pokemon.entry_number }} - {{ pokemon.pokemon_species.name }}
+    </li>
+  </ul>
 </template>
+
+<script setup>
+
+import { onMounted, computed, reactive, ref } from 'vue'
+
+const filterText = ref('')
+
+const pokeStore = reactive({
+  list: [],
+  filteredList: computed(() => 
+    pokeStore.list.filter(pokemon => 
+      pokemon.pokemon_species.name.includes(filterText.value)
+    )
+  )
+})
+
+
+onMounted(async () => {
+  const pokeData = await fetch('https:///pokeapi.co/api/v2/pokedex/2/').then(
+    response => response.json()
+  )
+  pokeStore.list = pokeData.pokemon_entries
+})
+
+
+</script>
 
 <style>
 #app {
@@ -18,4 +45,5 @@ import HelloWorld from './components/HelloWorld.vue'
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
