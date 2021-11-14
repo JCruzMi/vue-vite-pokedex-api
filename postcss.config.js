b78ff08442cdfdfee500cd7e1042770ce3229e1f
JCruzMi <47216265+JCruzMi@users.noreply.github.com>
2021-11-14 13:51:15 -0500

    try

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('tailwindcss')('tailwind.js'),
+        require('autoprefixer')()
+  ],
 }
