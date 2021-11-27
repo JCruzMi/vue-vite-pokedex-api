dd3bafa7bf6d8c998c81c30c4f47f4094aa4f93b
JCruzMi <47216265+JCruzMi@users.noreply.github.com>
2021-11-27 10:06:56 -0500

    up

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   }
-=======
-  mode: 'jit',
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
->>>>>>> 8ed44ca07c3345aad62762dab9530ae9554ffc5d
 }
